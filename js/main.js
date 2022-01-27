6054f459d771ab4f89642d3764aac8f5e3c35237
Lyudmila <88749276+ldubrovina@users.noreply.github.com>
2022-01-27 21:22:13 +0300

    Add slider

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const swiper = new Swiper('.swiper', {
+    slidesPerView: 3,
+    loop: true,
+    speed: 400,
+    spaceBetween:70,
+    centeredSlides:true,
+    slideToClickedSlide:true,
+    toggle:true,
+  });
